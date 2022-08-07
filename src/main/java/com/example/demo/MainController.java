dc4d7172ec1a1da409e3cbea76b9cde295a43106
Sudhakar <bssr93@gmail.com>
2022-08-07 20:00:33 +0530

    changes

--- src/main/java/com/example/demo/MainController.java
+++ src/main/java/com/example/demo/MainController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
-@GetMapping("/welcome")
+@GetMapping("/welcome/")
 public String index() {
     return "index.html";
   }
